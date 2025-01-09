55fddd6f176ec35113728b9803242ffbdb658cab
hayashubi94 <haya.shubi@gmail.com>
2025-01-09 12:40:36 +0200

    Initial commit

---
+++ TxEditor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TxEditor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
